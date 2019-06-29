80a2a7ed14896b3221ac1b72557e68d1cd900245
张yanbo <zhangyanbo1996@icloud.com>
2019-06-29 20:19:24 +0800

    下载模块

---
+++ GoogleStore/src/main/java/com/ybbbi/googlestore/moudle/DownloadMoudle.java
@@ -0,0 +1,20 @@
+package com.ybbbi.googlestore.moudle;
+
+import com.ybbbi.googlestore.R;
+import com.ybbbi.googlestore.bean.AppInfo;
+
+/**
+ * ybbbi
+ * 2019-06-29 20:01
+ */
+public class DownloadMoudle extends BaseMoudle<AppInfo> {
+    @Override
+    public int getLayout() {
+        return R.layout.download;
+    }
+
+    @Override
+    public void loadData(AppInfo info) {
+
+    }
+}
