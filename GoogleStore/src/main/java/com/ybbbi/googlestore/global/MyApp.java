ef4702ca343b387a2abc8f20b0c31503a43f21b0
张yanbo <zhangyanbo1996@icloud.com>
2019-06-30 21:20:40 +0800

    初始化utils

--- GoogleStore/src/main/java/com/ybbbi/googlestore/global/MyApp.java
+++ GoogleStore/src/main/java/com/ybbbi/googlestore/global/MyApp.java
@@ -6,19 +6,25 @@ import android.content.Context;
 import com.nostra13.universalimageloader.core.ImageLoader;
 import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
 
+import org.xutils.DbManager;
+import org.xutils.x;
+
 /*
  *ybbbi
  *2019-06-19 17:36
  */public class MyApp extends Application {
 
      public static Context context;
-
+        public static DbManager db;
 
     @Override
     public void onCreate() {
         super.onCreate();
+        x.Ext.init(this);
         context = getApplicationContext();
         ImageLoader.getInstance().init(ImageLoaderConfiguration.createDefault(this));
         //Log.i("MyApp", "onCreate: myapp");
+
     }
+
 }
