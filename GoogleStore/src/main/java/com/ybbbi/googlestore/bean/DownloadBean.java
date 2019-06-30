dc60e30f447ad753a8cd22f2bae71f6b0149b060
张yanbo <zhangyanbo1996@icloud.com>
2019-06-30 21:19:53 +0800

    更新bean类

--- GoogleStore/src/main/java/com/ybbbi/googlestore/bean/DownloadBean.java
+++ GoogleStore/src/main/java/com/ybbbi/googlestore/bean/DownloadBean.java
@@ -3,22 +3,32 @@ package com.ybbbi.googlestore.bean;
 import com.ybbbi.googlestore.global.MyApp;
 import com.ybbbi.googlestore.http.DownloadManager;
 
+import org.xutils.db.annotation.Column;
+import org.xutils.db.annotation.Table;
+
 import java.io.File;
 
 /**
  * ybbbi
  * 2019-06-29 16:03
  */
+    @Table(name = "ybbbi")
 public class DownloadBean {
+    @Column(name="state")
     public int state;
+    @Column(name="downloadUrl")
     public String downloadUrl;
+    @Column(name="currentProgress")
     public long currentProgress;
+    @Column(name="id",autoGen = false,isId = true)
     public int id;
+    @Column(name="Progress")
     public long Progress;
+    @Column(name="path")
     public String path;
     public static DownloadBean create(AppInfo info){
         DownloadBean bean=new DownloadBean();
-        bean.downloadUrl=info.downlaodUrl;
+        bean.downloadUrl=info.downloadUrl;
         bean.Progress=info.size;
         bean.id=info.id;
         bean.currentProgress=0;
