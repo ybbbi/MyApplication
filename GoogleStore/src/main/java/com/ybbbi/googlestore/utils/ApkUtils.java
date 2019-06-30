6de827826c81fb4b92100e90fd093ee4f9f8adbb
张yanbo <zhangyanbo1996@icloud.com>
2019-06-30 21:22:00 +0800

    低版本安装apk

--- GoogleStore/src/main/java/com/ybbbi/googlestore/utils/ApkUtils.java
+++ GoogleStore/src/main/java/com/ybbbi/googlestore/utils/ApkUtils.java
@@ -2,8 +2,13 @@ package com.ybbbi.googlestore.utils;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.os.Build;
+import android.support.v4.content.FileProvider;
+import android.util.Log;
 
 import com.ybbbi.googlestore.global.MyApp;
+
+import java.io.File;
 
 /**
  * ybbbi
@@ -11,9 +16,13 @@ import com.ybbbi.googlestore.global.MyApp;
  */
 public class ApkUtils {
     public static void install(String path){
-        Intent intent=new Intent(Intent.ACTION_VIEW);
-        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        intent.setDataAndType(Uri.parse("file://"+path),"application/vnd.android.package-archive");
+        File file =new File(path);
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+
+            intent.setDataAndType(Uri.fromFile(file), "application/vnd.android.package-archive");
+
         MyApp.context.startActivity(intent);
     }
 }
