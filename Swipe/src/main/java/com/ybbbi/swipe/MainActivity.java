ed086990b90163e23aae071c93d3201291acbf4b
张yanbo <zhangyanbo1996@icloud.com>
2019-07-08 10:34:20 +0800

    增加自定义点击时间与解决bug

--- Swipe/src/main/java/com/ybbbi/swipe/MainActivity.java
+++ Swipe/src/main/java/com/ybbbi/swipe/MainActivity.java
@@ -8,6 +8,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -27,39 +28,89 @@ public class MainActivity extends Activity {
         LinearLayoutManager layoutManager = new LinearLayoutManager(this);
         layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
         recyclerview.setLayoutManager(layoutManager);
-        recyclerview.setAdapter(new Adapter()) ;
-}
-       public class Adapter extends RecyclerView.Adapter<Adapter.Viewholder>{
 
-
+        recyclerview.setAdapter(new Adapter());
+        recyclerview.addOnScrollListener(new RecyclerView.OnScrollListener() {
             @Override
-            public Viewholder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-                View v=View.inflate(MainActivity.this,R.layout.recyclecontent,null);
-                return new Viewholder(v);
-            }
-
-           @Override
-           public void onBindViewHolder(@NonNull Adapter.Viewholder viewHolder, int i) {
-                viewHolder.tv_name.setText(Constants.NAMES[i]);
-           }
-
-
-           @Override
-            public int getItemCount() {
-                return Constants.NAMES.length;
-            }
-
-            class Viewholder extends RecyclerView.ViewHolder {
-
-                protected final TextView tv_name;
-
-                public Viewholder(@NonNull View itemView) {
-                    super(itemView);
-                    tv_name = itemView.findViewById(R.id.tv_name);
+            public void onScrollStateChanged(@NonNull RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+                if(currentLayout!=null){
+                    currentLayout.close();
                 }
             }
 
+            @Override
+            public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+            }
+        });
+    }
 
+    SwipeLayout currentLayout;
+
+    public class Adapter extends RecyclerView.Adapter<Adapter.Viewholder> {
+
+
+
+        @Override
+        public Viewholder onCreateViewHolder(ViewGroup viewGroup,  int i) {
+            View v = View.inflate(MainActivity.this, R.layout.recyclecontent, null);
+
+            return new Viewholder(v);
         }
 
+        @Override
+        public void onBindViewHolder(Viewholder viewHolder, final int i) {
+            viewHolder.tv_name.setText(Constants.NAMES[i]);
+            viewHolder.swipelayout.setOnSwipeListener(new SwipeLayout.OnSwipeListener() {
+
+                @Override
+                public void open(SwipeLayout layout) {
+                    if (currentLayout != null && layout != currentLayout) {
+                        //关闭前一个
+                        currentLayout.close();
+                    }
+                    currentLayout = layout;
+                }
+
+                @Override
+                public void close(SwipeLayout layout) {
+                    if (layout == currentLayout) {
+                        currentLayout = null;
+                    }
+                }
+            });
+            viewHolder.swipelayout.setonSwipeClickListener(new SwipeLayout.onSwipeClickListener() {
+                @Override
+                public void onClick() {
+
+                    Toast.makeText(MainActivity.this,"点击"+i+"个条目",Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+
+
+        @Override
+        public int getItemCount() {
+            return Constants.NAMES.length;
+        }
+
+
+
+        class Viewholder extends RecyclerView.ViewHolder {
+
+            @BindView(R.id.swipelayout)
+            SwipeLayout swipelayout;
+
+            protected final TextView tv_name;
+
+            public Viewholder(View itemView) {
+                super(itemView);
+                ButterKnife.bind(this, itemView);
+
+                tv_name = itemView.findViewById(R.id.tv_name);
+            }
+        }
     }
+
+}
